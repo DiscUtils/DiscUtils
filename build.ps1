2c8346e0e0a6446cca126931f8c1b31809bc9775
festive_ken <festive_ken@localhost>
2009-06-20 12:06:32 +0000

    Housekeeping - some updates to project files in trunk.

--- build.ps1
+++ build.ps1
@@ -2,7 +2,7 @@ $ver = "0.1"
 
 $basedir = "$pwd"
 $utilsdir = "C:\utils"
-$sbconsole = "C:\Program Files\EWSoftware\Sandcastle Help File Builder\SandcastleBuilderConsole.exe"
+$msbuild = "c:\windows\Microsoft.NET\Framework\v3.5\msbuild.exe"
 $vcsexpress = "C:\Program Files\Microsoft Visual Studio 9.0\Common7\IDE\VCSExpress.exe"
 
 # Clean up
@@ -15,4 +15,4 @@ del ${basedir}\help -recurse -force | out-null
 & ${vcsexpress} "${basedir}\DiscUtils.sln" /build Debug | out-null
 
 # Generate help
-& ${sbconsole} ${basedir}\Library.shfb
+& ${msbuild} ${basedir}\Library.shfbproj
