4a0d30ea3e25779f184987abbb0ddb6ab9baa0a1
festive_ken <festive_ken@localhost>
2009-08-16 12:11:53 +0000

    NTFS: Add ToString to NtfsOptions

--- src/Ntfs/NtfsOptions.cs
+++ src/Ntfs/NtfsOptions.cs
@@ -77,5 +77,14 @@ namespace DiscUtils.Ntfs
             get { return _hideDosFileNames; }
             set { _hideDosFileNames = value; }
         }
+
+        /// <summary>
+        /// Returns a string representation of the file system options.
+        /// </summary>
+        /// <returns>A string</returns>
+        public override string ToString()
+        {
+            return "Show: Normal " + (HideMetafiles ? "" : "Meta ") + (HideHiddenFiles ? "" : "Hidden ") + (HideSystemFiles ? "" : "System ") + (HideDosFileNames ? "" : "ShortNames ");
+        }
     }
 }
