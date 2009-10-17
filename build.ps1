cec32f5eb532f1c059b48ae307e29100a1dbd2f7
festive_ken <festive_ken@localhost>
2009-10-17 12:44:29 +0000

    Prep work for giving released versions of DiscUtils.dll a strong name.

--- build.ps1
+++ build.ps1
@@ -10,9 +10,20 @@ del ${basedir}\help -recurse -force | out-null
 & ${vcsexpress} "${basedir}\DiscUtils.sln" /clean Debug | out-null
 & ${vcsexpress} "${basedir}\DiscUtils.sln" /clean Release | out-null
 
+# Enable code signing
+$lines = Get-Content "${basedir}\src\DiscUtils.csproj";
+$lines | Foreach-Object { $_ -replace "<SignAssembly>.*</SignAssembly>", "<SignAssembly>true</SignAssembly>"} | Set-Content "${basedir}\src\DiscUtils.csproj";
+
+
 # Compile
 & ${vcsexpress} "${basedir}\DiscUtils.sln" /build Release | out-null
 & ${vcsexpress} "${basedir}\DiscUtils.sln" /build Debug | out-null
+
+
+# Disable code signing again
+$lines = Get-Content "${basedir}\src\DiscUtils.csproj";
+$lines | Foreach-Object { $_ -replace "<SignAssembly>.*</SignAssembly>", "<SignAssembly>false</SignAssembly>"} | Set-Content "${basedir}\src\DiscUtils.csproj";
+
 
 # Generate help
 & ${msbuild} ${basedir}\Library.shfbproj
