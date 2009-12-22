950f8fb8ba3217fc1dc396f4cf7138ba25d39dde
festive_ken <festive_ken@localhost>
2009-12-22 15:03:58 +0000

    Fix VHDCreate, where the help and quiet switches were duplicated.

--- utils/VHDCreate/Program.cs
+++ utils/VHDCreate/Program.cs
@@ -34,8 +34,6 @@ namespace VHDCreate
         private CommandLineParameter _destFile;
         private CommandLineSwitch _typeSwitch;
         private CommandLineSwitch _blockSizeSwitch;
-        private CommandLineSwitch _helpSwitch;
-        private CommandLineSwitch _quietSwitch;
 
         static void Main(string[] args)
         {
@@ -43,21 +41,17 @@ namespace VHDCreate
             program.Run(args);
         }
 
-        protected override ProgramBase.StandardSwitches DefineCommandLine(CommandLineParser parser)
+        protected override StandardSwitches DefineCommandLine(CommandLineParser parser)
         {
             _destFile = new CommandLineParameter("new.vhd", "Path to the VHD file to create.", false);
             _sourceFile = new CommandLineParameter("base.vhd", "For differencing disks, the path to the base disk.", true);
             _typeSwitch = new CommandLineSwitch("t", "type", "type", "The type of disk to create, one of: fixed, dynamic, diff.  The default is dynamic.");
             _blockSizeSwitch = new CommandLineSwitch("bs", "blocksize", "size", "For dynamic disks, the allocation uint size for new disk regions in bytes.  The default is 2MB.    Use B, KB, MB, GB to specify units (units default to bytes if not specified).");
-            _helpSwitch = new CommandLineSwitch(new string[] { "h", "?" }, "help", null, "Show this help.");
-            _quietSwitch = new CommandLineSwitch("q", "quiet", null, "Run quietly.");
 
             parser.AddParameter(_destFile);
             parser.AddParameter(_sourceFile);
             parser.AddSwitch(_typeSwitch);
             parser.AddSwitch(_blockSizeSwitch);
-            parser.AddSwitch(_helpSwitch);
-            parser.AddSwitch(_quietSwitch);
 
             return StandardSwitches.DiskSize;
         }
