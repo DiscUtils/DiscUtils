ba6c91a87af308bb2de66c54618d85a3f44e579c
festive_ken <festive_ken@localhost>
2011-01-30 13:10:39 +0000

    Add Ampersand(&) to list of permitted 8.3 characters.

--- src/Utilities.cs
+++ src/Utilities.cs
@@ -932,7 +932,7 @@ namespace DiscUtils
 
         public static bool Is8Dot3Char(char ch)
         {
-            return (ch >= 'A' && ch <= 'Z') || (ch >= '0' && ch <= '9') || "_^$~!#%£-{}()@'`".IndexOf(ch) != -1;
+            return (ch >= 'A' && ch <= 'Z') || (ch >= '0' && ch <= '9') || "_^$~!#%£-{}()@'`&".IndexOf(ch) != -1;
         }
 
         /// <summary>
