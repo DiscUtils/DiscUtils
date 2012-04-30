addb1d197a66e6839e2ee7a1c9813880793cacbc
festive_ken <festive_ken@localhost>
2012-04-30 01:42:23 +0100

    Tweak Crc32 comments.

--- src/Crc32Algorithm.cs
+++ src/Crc32Algorithm.cs
@@ -25,22 +25,22 @@ namespace DiscUtils
     internal enum Crc32Algorithm
     {
         /// <summary>
-        /// Used in Ethernet, PKZIP, BZIP2, Gzip, PNG, etc.
+        /// Used in Ethernet, PKZIP, BZIP2, Gzip, PNG, etc. (aka CRC32)
         /// </summary>
         Common = 0,
 
         /// <summary>
-        /// Used in iSCSI, SCTP, Btrfs, Vhdx.
+        /// Used in iSCSI, SCTP, Btrfs, Vhdx. (aka CRC32C)
         /// </summary>
         Castagnoli = 1,
 
         /// <summary>
-        /// Unknown usage.
+        /// Unknown usage. (aka CRC32K)
         /// </summary>
         Koopman = 2,
 
         /// <summary>
-        /// Used in AIXM.
+        /// Used in AIXM.  (aka CRC32Q)
         /// </summary>
         Aeronautical = 3
     }
