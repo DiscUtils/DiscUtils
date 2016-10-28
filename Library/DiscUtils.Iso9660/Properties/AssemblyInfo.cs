d286a42e1847c8ded2e8c36e3d685655b9529789
Michael Bisbjerg <michael@mbwarez.dk>
2016-10-28 21:08:44 +0200

    Move Is9660 out of DiscUtils

---
+++ Library/DiscUtils.Iso9660/Properties/AssemblyInfo.cs
@@ -0,0 +1,21 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("DiscUtils.Iso9660")]
+[assembly: AssemblyTrademark("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("1c0dd4d8-3db3-4630-8253-ea8566686225")]
+
+[assembly: InternalsVisibleTo("DiscUtils.Udf, PublicKey=0024000004800000940000000602000000240000525341310004000001000100756aae5766e7db6f0f4f0702b20a4517d80a80ed735bea8c65e91d3f6210134163e1724f7a3d746108ead8e97ffcf04a4a7d813c506dd8abf705a8f557caf3f957434e932cd0e23d497672a23986109a7c3e7b6ab93618d30148f468044aa853f4a494bead3d35c08f1728bc2dc8895bf6a0e82895a26078d8ca4d8f48397fa5")]
