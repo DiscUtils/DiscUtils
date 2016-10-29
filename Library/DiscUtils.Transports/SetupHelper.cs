547ffcabd57abd3e7eb174a12b049411a71cf3f9
Michael Bisbjerg <michael@mbwarez.dk>
2016-10-30 00:27:39 +0200

    Add Transports and Containers MetaPackages

---
+++ Library/DiscUtils.Transports/SetupHelper.cs
@@ -0,0 +1,14 @@
+﻿using DiscUtils.CoreCompat;
+
+namespace DiscUtils.Transports
+{
+    public static class SetupHelper
+    {
+        public static void SetupTransports()
+        {
+            Setup.SetupHelper.RegisterAssembly(ReflectionHelper.GetAssembly(typeof(Iscsi.Disk)));
+            Setup.SetupHelper.RegisterAssembly(ReflectionHelper.GetAssembly(typeof(Nfs.NfsFileSystem)));
+            Setup.SetupHelper.RegisterAssembly(ReflectionHelper.GetAssembly(typeof(OpticalDisk.Disc)));
+        }
+    }
+}
