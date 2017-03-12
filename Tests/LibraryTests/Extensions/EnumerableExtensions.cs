92696f5453e1238086fcc25a06efcc1e06836057
Glen Parker <glenebob@gmail.com>
2017-03-12 15:30:56 -0700

    Visual Studio 2017 Create seperate .sln files for VS2015 and VS2017. Upgrade the VS2017 solution. Add all new .csproj files project, and retain the .xproj and .json project files. Fix the Apple sample data test by making it look more aggressively for the test .iso file.

---
+++ Tests/LibraryTests/Extensions/EnumerableExtensions.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+
+namespace LibraryTests.Extensions
+{
+    public static class EnumerableExtensions
+    {
+        public static IEnumerable<T> AsSingle<T>(this T item)
+        {
+            yield return item;
+        }
+
+        public static IEnumerable<T> Concat<T>(this IEnumerable<T> items, params T[] additionalItems)
+        {
+            foreach (T item in items)
+            {
+                yield return item;
+            }
+
+            foreach (T item in additionalItems)
+            {
+                yield return item;
+            }
+        }
+    }
+}
