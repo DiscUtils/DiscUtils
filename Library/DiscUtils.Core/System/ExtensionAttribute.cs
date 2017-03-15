909ccf0c1a9d58f03d37b24e528d1e32a429010a
Glen Parker <glenebob@gmail.com>
2017-03-14 17:36:12 -0700

    Warnings Kill all warnings by adding XML comments and one warning suppression.

--- Library/DiscUtils.Core/System/ExtensionAttribute.cs
+++ Library/DiscUtils.Core/System/ExtensionAttribute.cs
@@ -1,6 +1,10 @@
 ﻿#if NET20
 namespace System.Runtime.CompilerServices
 {
+    /// <summary>
+    /// Indicates that a method is an extension method, or that a class or assembly contains
+    /// extension methods.
+    /// </summary>
     [AttributeUsage(AttributeTargets.Assembly | AttributeTargets.Class | AttributeTargets.Method)]
     public sealed class ExtensionAttribute : Attribute { }
 }
