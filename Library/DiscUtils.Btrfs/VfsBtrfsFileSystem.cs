0d1199d5e35c54413f1d5c96d1ef4bbfb5427460
Bianco Veigel <bianco.veigel@ahd.de>
2017-04-20 20:02:48 +0200

    initial btrfs (with size support only)
    # Conflicts:
    #	DiscUtils.sln

---
+++ Library/DiscUtils.Btrfs/VfsBtrfsFileSystem.cs
@@ -0,0 +1,95 @@
+//
+// Copyright (c) 2017, Bianco Veigel
+//
+// Permission is hereby granted, free of charge, to any person obtaining a
+// copy of this software and associated documentation files (the "Software"),
+// to deal in the Software without restriction, including without limitation
+// the rights to use, copy, modify, merge, publish, distribute, sublicense,
+// and/or sell copies of the Software, and to permit persons to whom the
+// Software is furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+// FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+// DEALINGS IN THE SOFTWARE.
+//
+
+using System;
+using System.IO;
+using DiscUtils.Internal;
+using DiscUtils.Vfs;
+
+namespace DiscUtils.Btrfs
+{
+    internal sealed class VfsBtrfsFileSystem :VfsReadOnlyFileSystem<VfsDirEntry, IVfsFile, IVfsDirectory<VfsDirEntry,IVfsFile>, Context>
+    {
+        public VfsBtrfsFileSystem(Stream stream)
+            :base(new DiscFileSystemOptions())
+        {
+            Context = new Context
+            {
+                RawStream = stream,
+            };
+            foreach (var offset in BtrfsFileSystem.SuperblockOffsets)
+            {
+                if (offset + SuperBlock.Length > stream.Length) continue;
+
+                stream.Position = offset;
+                var superblockData = Utilities.ReadFully(stream, SuperBlock.Length);
+                var superblock = new SuperBlock();
+                superblock.ReadFrom(superblockData, 0);
+
+                if (superblock.Magic != SuperBlock.BtrfsMagic)
+                    throw new IOException("Invalid Superblock Magic");
+
+                if (Context.SuperBlock == null)
+                    Context.SuperBlock = superblock;
+                else if (Context.SuperBlock.Generation < superblock.Generation)
+                    Context.SuperBlock = superblock;
+            }
+        }
+        
+        public override string FriendlyName
+        {
+            get { return "Btrfs"; }
+        }
+
+        /// <inheritdoc />
+        public override string VolumeLabel { get { return Context.SuperBlock.Label; } }
+        
+        /// <summary>
+        /// Size of the Filesystem in bytes
+        /// </summary>
+        public override long Size
+        {
+            get { return (long) Context.SuperBlock.TotalBytes; }
+        }
+
+        /// <summary>
+        /// Used space of the Filesystem in bytes
+        /// </summary>
+        public override long UsedSpace
+        {
+            get { return (long)Context.SuperBlock.BytesUsed; }
+        }
+
+        /// <summary>
+        /// Available space of the Filesystem in bytes
+        /// </summary>
+        public override long AvailableSpace
+        {
+            get { return Size - UsedSpace; }
+        }
+
+        protected override IVfsFile ConvertDirEntryToFile(VfsDirEntry dirEntry)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
