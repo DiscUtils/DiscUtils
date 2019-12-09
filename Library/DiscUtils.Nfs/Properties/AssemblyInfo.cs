8b420a1221f03d265aac71b90d0a6fa4b337171b
Frederik Carlier <frederik.carlier@quamotion.mobi>
2019-12-09 11:27:34 +0100

    Use auto-generated assembly attributes

--- Library/DiscUtils.Nfs/Properties/AssemblyInfo.cs
+++ Library/DiscUtils.Nfs/Properties/AssemblyInfo.cs
@@ -1,15 +1,6 @@
-﻿using System.Reflection;
-using System.Runtime.CompilerServices;
+﻿using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
-
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("DiscUtils.Nfs")]
-[assembly: AssemblyTrademark("")]
 [assembly: InternalsVisibleTo("LibraryTests, PublicKey=002400000480000094000000060200000024000052534131000400000100010047ebec172a9831bb20fede77e17d784026ea7030d7055f2ae09576c71cebe77ebfab436d80580a4fcbba7242ff61bd52b686f5fe9d41fe7cd3e6c05b8a876eccf35b8ad7c5e3a6704295d7210b138d7280a6f72688419a65dd7a8612d66869f2e712c57c41fcc9196e4cb06d95d8e678f6967e65348c370405fb7eeb6aa1d3e8")]
 
 // Setting ComVisible to false makes the types in this assembly not visible
