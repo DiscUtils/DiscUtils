ecb6421e89f1d2a9ca1f695a18ff3e127b0ebb5c
Frederik Carlier <frederik.carlier@quamotion.mobi>
2021-04-21 10:58:33 +0200

    Mark DiscUtils.NTfs as SupportedOSPlatform("windows")
    `Quotas.cs` uses `SecurityIdentifier` which is marked as Windows-only, and this constraint eventually bubbles up to the entire assembly.

--- Library/DiscUtils.Ntfs/Properties/AssemblyInfo.cs
+++ Library/DiscUtils.Ntfs/Properties/AssemblyInfo.cs
@@ -1,10 +1,18 @@
 ﻿using System.Runtime.InteropServices;
 
+#if NET5_0_OR_GREATER
+using System.Runtime.Versioning;
+#endif
+
 // Setting ComVisible to false makes the types in this assembly not visible
 // to COM components.  If you need to access a type in this assembly from
 // COM, set the ComVisible attribute to true on that type.
 
 [assembly: ComVisible(false)]
+
+#if NET5_0_OR_GREATER
+[assembly: SupportedOSPlatform("windows")]
+#endif
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 
